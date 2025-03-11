011e8daaacd3ba940c9eece51d26c7def1face55
wwh <1536322061@qq.com>
2025-03-11 09:04:37 +0800

    新增微信测试公众号推送

---
+++ wechatSend.py
@@ -0,0 +1,37 @@
+import json
+import requests
+import os
+
+
+appID = os.environ["AppID"]
+appSecret = os.environ["AppSecret"]
+openId = os.environ["OpenId"]
+templateId = os.environ["TemplateId"]
+
+def get_access_token():
+    # 获取access token的url
+    url = 'https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid={}&secret={}' \
+        .format(appID.strip(), appSecret.strip())
+    response = requests.get(url).json()
+    # print(response)
+    access_token = response.get('access_token')
+    return access_token
+
+def send_weather(access_token):
+    # touser 就是 openID
+    # template_id 就是模板ID
+    # url 就是点击模板跳转的url
+    # data就按这种格式写，time和text就是之前{{time.DATA}}中的那个time，value就是你要替换DATA的值
+
+    body = {
+        "touser": openId.strip(),
+        "template_id": templateId.strip(),
+        "url": "https://weixin.qq.com",
+        "data": {
+        }
+    }
+    url = 'https://api.weixin.qq.com/cgi-bin/message/template/send?access_token={}'.format(access_token)
+    print(requests.post(url, json.dumps(body)).text)
+
+if __name__ == "__main__":
+    send_weather(get_access_token())
